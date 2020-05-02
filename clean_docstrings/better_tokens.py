af8a40ad8c94730fc6fbfef367d4677ffe07f752
Micha Selak <micha.selak@student.hs-rm.de>
2020-05-02 21:30:00 +0200

    mostly refactoring

--- clean_docstrings/better_tokens.py
+++ clean_docstrings/better_tokens.py
@@ -6,42 +6,80 @@ from cleantext import clean
 import argparse
 import pathlib as pl
 
-# TODO: more testing, give directory instead of file as option, output_file=input file?, test regex's more, ...
+# TODO: more testing(generics in java, other languages than java, python),
+#  test regex's more, lower cases tokens not sure how or if it is a problem, ...
 
 
 def argument_parser():
     parser = argparse.ArgumentParser(description="creates better_docstring_tokens from docstring")
-    parser.add_argument('-i', '--input_file', type=str, help="Input file", required=True)
-    parser.add_argument('-o', '--output_file', type=str, help="Output file", required=True)
+    parser.add_argument('-i', '--input', type=str, help="Input file", required=True)
+    parser.add_argument('-o', '--output_file', type=str, help="Output file (can only be chosen when input is a file")
     parser.add_argument('-l', '--language', type=str, help="language", default="java")
     return parser
 
 
 def main(args):
-    # input = pl.Path(args.input_dir)
-    # output = pl.Path(args.output_dir)
-    with open(args.input_file, mode="rt") as input:
-        lines = input.readlines()
-    with open(args.output_file, mode="wt") as output:
-        for line in lines:
-            dic = json.loads(line)
-            docstring = dic["docstring"]
+    input = pl.Path(args.input)
+    if input.is_dir():
+        input_files = input.glob('**/*.jsonl')
+        parse_files(input_files, args.language)
 
-            if args.language == "java":
-                docstring = remove_comment_delimiters(docstring)
+    elif args.output_file:
+        output_path = pl.Path(args.output_file)
+        parse_files([input], args.language, output_path)
 
-            summary = get_summary(docstring)
-            summary = remove_html_tags(summary)
+    else:
+        parse_files([input], args.language)
 
-            if args.language == "java":
-                summary = remove_javadoc_tags(summary)
 
-            summary = remove_urls(summary)
-            tokens = get_tokens(summary)
-            print(f"{dic['docstring']}\nnew token:\n{tokens}\nold tokens:\n{dic['docstring_tokens']}\n\n")
-            """dic["better_docstring_tokens"] = tokens
-            result = json.dumps(dic)
-            output.write(result + "\n")"""
+def get_language(file_name: str):
+    languages = ["java", "python", "go", "ruby", "php", "javascript"]
+    for language in languages:
+        if language in file_name:
+            return language
+    assert False, "Can't detect language"
+
+
+def parse_files(input, language: str, output_file: pl.Path = None):
+    for file_path in input:
+        print(f"parsing {file_path}")
+        with file_path.open(mode="rt") as file:
+            lines = file.readlines()
+
+        if not language:
+            language = get_language(file_path.stem)
+
+        if output_file is None:
+            output_path = file_path
+        else:
+            output_path = output_file
+
+        with output_path.open(mode="wt") as output:
+            parsed_lines = parse_lines(lines, language)
+            for dic in parsed_lines:
+                json_line = json.dumps(dic) + "\n"
+                output.write(json_line)
+
+
+def parse_lines(lines: list, lang: str):
+    for line in lines:
+        dic = json.loads(line)
+        docstring = dic["docstring"]
+
+        if lang == "java":
+            docstring = remove_comment_delimiters(docstring)
+
+        summary = get_summary(docstring)
+        summary = remove_html_tags(summary)
+
+        if lang == "java":
+            summary = remove_javadoc_tags(summary)
+
+        summary = remove_urls(summary)
+        tokens = get_tokens(summary)
+        dic["better_docstring_tokens"] = tokens
+        # print(f"{dic['docstring']}\nnew token:\n{tokens}\nold tokens:\n{dic['docstring_tokens']}\n\n")
+        yield dic
 
 
 # delimiters
@@ -70,25 +108,6 @@ def remove_html_tags(text: str) -> str:
     return soup.get_text()
 
 
-"""
-@author
-@version
-@since
-@see
-@serial
-@serialField
-@return
-@exception
-@throws
-@deprecated
-{@inheritDoc}
-{@link reference}
-{@linkPlain reference}
-{@value}
-{@docRoot}
-{@literal}
-{@code}
-"""
 BAD_JAVADOC_TAGS_REGEX = \
     re.compile(r"@((author)|(version)|(since)|(see)|(serial)|(serialField)|(return)|(exception)|(throws)|(deprecated)|"
                r"(inheritDoc)|(link)|(linkPlain)|(value)|(docRoot)|(literal)|(code))")
@@ -103,7 +122,9 @@ def remove_urls(summary: str) -> str:
 
 
 DOCSTRING_REGEX_TOKENIZER = \
-    re.compile(r"[^\s,'\"`.():\[\]=*;>{\}+-/\\]+|\\+|\.+|\(\)|{\}|\[\]|\(+|\)+|:+|\[+|\]+|{+|\}+|=+|\*+|;+|>+|\++|-+|/+")
+    re.compile(
+        r"[^\s,'\"`.():\[\]=*;>{\}+-/\\]+|\\+|\.+|\(\)|{\}|\[\]|\(+|\)+|:+|\[+|\]+|{+|\}+|=+|\*+|;+|>+|\++|-+|/+"
+    )
 
 
 def get_tokens(summary: str) -> list:
